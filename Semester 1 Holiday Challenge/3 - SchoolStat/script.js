b0087dd7de3f26ba06054945d551489b250b6ce0
Tolu Falana <falanaft@gmail.com>
2022-11-08 03:50:27 +0100

    Update index.html

--- Semester 1 Holiday Challenge/3 - SchoolStat/script.js
+++ Semester 1 Holiday Challenge/3 - SchoolStat/script.js
@@ -5,7 +5,7 @@
     
     switching = true;
     dir = "ascending";
-
+    
     while (switching) {
       switching = false;
       rows = table.getElementsByTagName("tr");
@@ -39,4 +39,23 @@
         }
       }
     }
+    //Change sortable symbol based on sort direction
+    let col = document.querySelectorAll("th i")
+    // let cols = Array.from(col)
+    // let colSymbol = cols.filter(item => item.className != "")
+    col.forEach( i => {if (i.className != "") {i.className = 'bx bxs-sort-alt'}})
+
+    if (dir == "ascending") {
+      table.querySelectorAll("i")[n].className = 'bx bx-caret-up'
+    } else if (dir == "descending") {      
+      table.querySelectorAll("i")[n].className = 'bx bx-caret-down'
+    }
   }
+  
+
+//Add event handlers for sortable columns
+document.querySelectorAll('.table-1 th')[0].addEventListener('click', () => {sortTable('table-1', 0);})
+document.querySelectorAll('.table-1 th')[1].addEventListener('click', () => {sortTable('table-1', 1);})
+document.querySelectorAll('.table-1 th')[2].addEventListener('click', () => {sortTable('table-1', 2);})
+document.querySelectorAll('.table-2 th')[1].addEventListener('click', () => {sortTable('table-2', 1);})
+document.querySelectorAll('.table-3 th')[1].addEventListener('click', () => {sortTable('table-3', 1);})
